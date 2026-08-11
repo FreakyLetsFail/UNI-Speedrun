8bad28543c8299e2f02fe2315cb52c3339c6d942
Justus W. <65566771+FreakyLetsFail@users.noreply.github.com>
2026-08-11 10:10:31 +0200

    added warte_auf_ergebnis

--- src/uni_speedrun/fachmodell/modulstatus.py
+++ src/uni_speedrun/fachmodell/modulstatus.py
@@ -3,5 +3,5 @@ from enum import Enum
 class Modulstatus(Enum):
     GEPLANT = "geplant"
     AKTIV = "aktiv"
-    WARTE_AUF_ERGEBNIS ? "warte_auf_ergebnis"
+    WARTE_AUF_ERGEBNIS = "warte_auf_ergebnis"
     ABGESCHLOSSEN = "abgeschlossen"
